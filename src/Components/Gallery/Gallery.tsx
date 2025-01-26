71fafac327c3383f23218776f5412691b27dd085
unknown <sunebarikara@gmail.com>
2025-01-26 15:24:31 +0100

    core features added

---
+++ src/Components/Gallery/Gallery.tsx
@@ -0,0 +1,19 @@
+
+
+const Gallery = () => {
+  return (
+    <>
+    <div><h1>Dynamic Gallery</h1></div>
+    <div className="container">
+      <div className="gallery">
+        <div className="img-box"><h3>Nigeria</h3></div>
+        <div className="img-box"><h3>China</h3></div>
+        <div className="img-box"><h3>India</h3></div>
+        <div className="img-box"><h3>Dubia</h3></div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Gallery
