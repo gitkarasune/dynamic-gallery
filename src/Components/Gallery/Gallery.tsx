6dc4f8b54e03ce2fdb068b8ac4f6d74ca081293d
unknown <sunebarikara@gmail.com>
2025-01-26 15:37:17 +0100

    Updated the footer

--- src/Components/Gallery/Gallery.tsx
+++ src/Components/Gallery/Gallery.tsx
@@ -1,19 +1,34 @@
-
-
 const Gallery = () => {
+
+  const currentYear = new Date().getFullYear();
+
   return (
     <>
-    <div><h1>Dynamic Gallery</h1></div>
-    <div className="container">
-      <div className="gallery">
-        <div className="img-box"><h3>Nigeria</h3></div>
-        <div className="img-box"><h3>China</h3></div>
-        <div className="img-box"><h3>India</h3></div>
-        <div className="img-box"><h3>Dubia</h3></div>
+      <div>
+        <h1>Dynamic Gallery</h1>
       </div>
-    </div>
-    </>
-  )
-}
+      <div className="container">
+        <div className="gallery">
+          <div className="img-box">
+            <h3>Nigeria</h3>
+          </div>
+          <div className="img-box">
+            <h3>China</h3>
+          </div>
+          <div className="img-box">
+            <h3>India</h3>
+          </div>
+          <div className="img-box">
+            <h3>Dubia</h3>
+          </div>
+        </div>
+      </div>
 
-export default Gallery
+      <footer className="footer">
+        <p>&copy; {currentYear} <a href="https://karasune.vercel.app" target="_blank" rel="noopener noreferrer" className="kendev">kendev</a></p>
+      </footer>
+    </>
+  );
+};
+
+export default Gallery;
